5d934a926c94d00c82855330cd8c2dba8b911472
baedirin <bdavis11@seattlecentral.edu>
2022-08-25 16:43:54 -0700

    working on udemy class

--- stacks.py
+++ stacks.py
@@ -2,33 +2,90 @@
 
 # Use the "end" for adding and removing with a list, because O(1)
 
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+#
+#
+# class Stack:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.top = new_node
+#         self.height = 1
+#
+#     def print_stack(self):
+#         temp = self.top
+#         while temp is not None:
+#             print(temp.value)
+#             temp = temp.next
+#
+#     def push(self, value):
+#         new_node = Node(value)
+#         if self.height == 0:
+#             self.top == new_node
+#         else:
+#             new_node.next = self.top
+#             self.top = new_node
+#         self.height += 1
+#
+#     def pop(self):
+#         if self.height == 0:
+#             return None
+#         temp = self.top
+#         self.top = self.top.next
+#         temp.next = None
+#         self.height -= 1
+#         return temp
+
+# Queue: add on one end, remove on another end
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
 
-class Stack:
+class Queue:
     def __init__(self, value):
         new_node = Node(value)
-        self.top = new_node
-        self.height = 1
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
 
-    def print_stack(self):
-        temp = self.top
+    def print_queue(self):
+        temp = self.first
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
-    def push(self, value):
+    def enqueue(self, value):
         new_node = Node(value)
-        if self.height == 0:
-            self.top == new_node
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
         else:
-            new_node.next = self.top
-            self.top = new_node
-        self.height += 1
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
 
-my_stack = Stack(4)
-my_stack.push(10)
-my_stack.print_stack()
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+
